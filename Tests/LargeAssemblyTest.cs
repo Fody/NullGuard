ee38ab68f7866001fd576ada0d64c9f98b3927ac
Simon Cropp <simon.cropp@gmail.com>
2013-01-12 16:42:27 +1100

    bump version. make large tests optional

--- Tests/LargeAssemblyTest.cs
+++ Tests/LargeAssemblyTest.cs
@@ -7,32 +7,22 @@ using NUnit.Framework;
 [TestFixture]
 public class LargeAssemblyTest
 {
-    [TestCase("Esent.Interop")]
-    [TestCase("GeoAPI")]
-    [TestCase("ICSharpCode.NRefactory.CSharp")]
-    [TestCase("ICSharpCode.NRefactory")]
-    [TestCase("Jint.Raven")]
-    [TestCase("log4net")]
-    [TestCase("Lucene.Net.Contrib.Spatial.NTS")]
-    [TestCase("Lucene.Net")]
-    [TestCase("Mono.Cecil")]
-    [TestCase("Mono.Cecil.Mdb")]
-    [TestCase("Mono.Cecil.Pdb")]
-    [TestCase("Mono.Cecil.Rocks")]
-    [TestCase("NetTopologySuite")]
-    [TestCase("NServiceBus.Core")]
-    [TestCase("NServiceBus")]
-    [TestCase("NullGuard")]
-    [TestCase("NullGuard.Fody")]
-    [TestCase("nunit.framework")]
-    [TestCase("PowerCollections")]
-    [TestCase("Raven.Abstractions")]
-    [TestCase("Raven.Database")]
-    [TestCase("Spatial4n.Core.NTS")]
-    [TestCase("Tests")]
-    public void VerifyAssembly(string assemblyName)
+    [Test]
+    [Ignore]
+    public void ProcesAllAssemblies()
     {
-        var assemblyPath = Path.Combine(Environment.CurrentDirectory, assemblyName + ".dll");
+        foreach (var assemblyPath in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.dll"))
+        {
+            if (assemblyPath.EndsWith("2.dll") || assemblyPath.EndsWith("3.dll"))
+            {
+                continue;
+            }
+            VerifyAssembly(assemblyPath);
+        }
+    }
+
+    public void VerifyAssembly(string assemblyPath)
+    {
 
         Debug.WriteLine("Verifying " + assemblyPath);
         var cleanAssembly = assemblyPath.Replace(".dll", "2.dll");
