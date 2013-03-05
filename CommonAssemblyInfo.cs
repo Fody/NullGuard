d04325091da44673d2075d2a1b7f00e1a7911428
Cameron MacFarland <distantcam@gmail.com>
2013-03-06 00:19:42 +0800

    version bump

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("NullGuard")]
 [assembly: AssemblyProduct("NullGuard")]
-[assembly: AssemblyVersion("0.3.1.0")]
-[assembly: AssemblyFileVersion("0.3.1.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
