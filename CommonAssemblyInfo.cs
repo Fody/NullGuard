c2168ddd382aadf63dde84e8276d301566a96118
Simon Cropp <simon.cropp@gmail.com>
2013-04-02 22:06:41 +1100

    refs

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("NullGuard")]
 [assembly: AssemblyProduct("NullGuard")]
-[assembly: AssemblyVersion("0.5.1.1")]
-[assembly: AssemblyFileVersion("0.5.1.1")]
+[assembly: AssemblyVersion("0.5.1.2")]
+[assembly: AssemblyFileVersion("0.5.1.2")]
