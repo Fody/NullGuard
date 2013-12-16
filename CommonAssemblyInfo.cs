6bbddbcbff36183e92fa666a0e90896e789ba18f
Justin Thirkell <justin.thirkell@xero.com>
2013-12-16 13:32:54 +1300

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NullGuard")]
 [assembly: AssemblyProduct("NullGuard")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
