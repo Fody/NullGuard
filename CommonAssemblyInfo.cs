1397380ed39cd447b4355e1acf43e4f4cd22ef17
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:20:48 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NullGuard")]
 [assembly: AssemblyProduct("NullGuard")]
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
