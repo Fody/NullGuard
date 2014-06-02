3a0206ae885e7a82fda29a6593635b38612c911b
Cameron MacFarland <distantcam@gmail.com>
2014-06-02 23:15:10 +0800

    Fixed duplicate decompilers

--- Tests/ApprovedTests.cs
+++ Tests/ApprovedTests.cs
@@ -1,13 +1,8 @@
 ﻿#if(DEBUG)
 
-using System;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using ApprovalTests;
 using ApprovalTests.Reporters;
-using Microsoft.Build.Utilities;
 using NUnit.Framework;
 
 [TestFixture]
@@ -17,79 +12,79 @@ public class ApprovedTests
     [Test]
     public void ClassWithBadAttributes()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "ClassWithBadAttributes"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "ClassWithBadAttributes"));
     }
 
     [Test]
     public void ClassWithPrivateMethod()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "ClassWithPrivateMethod"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "ClassWithPrivateMethod"));
     }
 
     [Test]
     public void ClassWithPrivateMethodNoAssert()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "ClassWithPrivateMethod"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "ClassWithPrivateMethod"));
     }
 
     [Test]
     public void GenericClass()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "GenericClass`1"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "GenericClass`1"));
     }
 
     [Test]
     public void GenericClassNoAssert()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "GenericClass`1"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "GenericClass`1"));
     }
 
     [Test]
     public void Indexers()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "Indexers"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "Indexers"));
     }
 
     [Test]
     public void InterfaceBadAttributes()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "InterfaceBadAttributes"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "InterfaceBadAttributes"));
     }
 
     [Test]
     public void SimpleClass()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "SimpleClass"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "SimpleClass"));
     }
 
     [Test]
     public void SimpleClassNoAssert()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "SimpleClass"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "SimpleClass"));
     }
 
     [Test]
     public void SkipIXamlMetadataProvider()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "XamlMetadataProvider"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "XamlMetadataProvider"));
     }
 
     [Test]
     public void SpecialClass()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "SpecialClass"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "SpecialClass"));
     }
 
     [Test]
     public void SpecialClassNoAssert()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "SpecialClass"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[1], "SpecialClass"));
     }
 
     [Test]
     public void UnsafeClass()
     {
-        Approvals.Verify(Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "UnsafeClass"));
+        Approvals.Verify(Decompiler.Decompile(AssemblyWeaver.AfterAssemblyPaths[0], "UnsafeClass"));
     }
 
     [Test]
@@ -108,57 +103,6 @@ public class ApprovedTests
     public void ErrorsList()
     {
         Approvals.VerifyAll(AssemblyWeaver.Errors.OrderBy(e => e), "Errors: ");
-    }
-
-    private static string Decompile(string assemblyPath, string identifier = "")
-    {
-        var exePath = GetPathToILDasm();
-
-        if (!string.IsNullOrEmpty(identifier))
-            identifier = "/item:" + identifier;
-
-        using (var process = Process.Start(new ProcessStartInfo(exePath, String.Format("\"{0}\" /text /linenum {1}", assemblyPath, identifier))
-        {
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        }))
-        {
-            var projectFolder = Path.GetFullPath(Path.GetDirectoryName(assemblyPath) + "\\..\\..\\..").Replace("\\", "\\\\");
-            projectFolder = String.Format("{0}{1}\\\\", Char.ToLower(projectFolder[0]), projectFolder.Substring(1));
-
-            process.WaitForExit(10000);
-
-            string ilText = string.Join(Environment.NewLine, Regex.Split(process.StandardOutput.ReadToEnd(), Environment.NewLine)
-                    .Where(l => !l.StartsWith("// ") && !string.IsNullOrEmpty(l))
-                    .Select(l => l.Replace(projectFolder, ""))
-                    .ToList());
-
-            // Sort the custom attributes in the generated IL code because the order of custom attributes is not specified (see
-            // http://stackoverflow.com/questions/480007) and not stable.
-            ilText = Regex.Replace(
-                    ilText,
-                    @"(?<CustomInstance>[\t ]*\.custom instance void.*=[^)]+\).*((\r\n)|\n){1}){2,}",
-                    match => string.Join("", match.Groups["CustomInstance"].Captures.Cast<Capture>().Select(x => x.Value).OrderBy(x => x.Trim())));
-
-            // Getters and setters seem to be swapping order sometimes
-            ilText = Regex.Replace(
-                ilText,
-                @"(?<getset>[\t ]*\.[gs]et instance [^)]+\).*((\r\n)|\n){1}){2}",
-                match => string.Join("", match.Groups["getset"].Captures.Cast<Capture>().Select(x => x.Value).OrderBy(x => x.Trim())));
-
-            return ilText;
-        }
-    }
-
-    private static string GetPathToILDasm()
-    {
-        var path = Path.Combine(ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.Version40), @"bin\NETFX 4.0 Tools\ildasm.exe");
-        if (!File.Exists(path))
-            path = path.Replace("v7.0", "v8.0");
-        if (!File.Exists(path))
-            Assert.Ignore("ILDasm could not be found");
-        return path;
     }
 }
 
--- Tests/Helpers/Decompiler.cs
+++ Tests/Helpers/Decompiler.cs
@@ -27,10 +27,25 @@ public static class Decompiler
 
             process.WaitForExit(10000);
 
-            return string.Join(Environment.NewLine, Regex.Split(process.StandardOutput.ReadToEnd(), Environment.NewLine)
+            string ilText = string.Join(Environment.NewLine, Regex.Split(process.StandardOutput.ReadToEnd(), Environment.NewLine)
                     .Where(l => !l.StartsWith("// ") && !string.IsNullOrEmpty(l))
                     .Select(l => l.Replace(projectFolder, ""))
                     .ToList());
+
+            // Sort the custom attributes in the generated IL code because the order of custom attributes is not specified (see
+            // http://stackoverflow.com/questions/480007) and not stable.
+            ilText = Regex.Replace(
+                    ilText,
+                    @"(?<CustomInstance>[\t ]*\.custom instance void.*=[^)]+\).*((\r\n)|\n){1}){2,}",
+                    match => string.Join("", match.Groups["CustomInstance"].Captures.Cast<Capture>().Select(x => x.Value).OrderBy(x => x.Trim())));
+
+            // Getters and setters seem to be swapping order sometimes
+            ilText = Regex.Replace(
+                ilText,
+                @"(?<getset>[\t ]*\.[gs]et instance [^)]+\).*((\r\n)|\n){1}){2}",
+                match => string.Join("", match.Groups["getset"].Captures.Cast<Capture>().Select(x => x.Value).OrderBy(x => x.Trim())));
+
+            return ilText;
         }
     }
 
