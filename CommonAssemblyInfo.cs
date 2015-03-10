e1640ef51faecb50242be84a41136dbe1688caba
Simon Cropp <simon.cropp@gmail.com>
2015-03-11 08:42:14 +1100

    SN

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("NullGuard")]
 [assembly: AssemblyProduct("NullGuard")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyFileVersion("1.4.0")]
