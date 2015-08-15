2a22688cf905e9da687fbec16eab975f223d4f7b
Simon Cropp <simon.cropp@gmail.com>
2015-08-15 17:58:19 +1000

    fix nuspec name

--- Fody/ReferenceFinder.cs
+++ Fody/ReferenceFinder.cs
@@ -15,7 +15,7 @@ public static class ReferenceFinder
         var baseLib = assemblyResolver.Resolve("mscorlib");
         var baseLibTypes = baseLib.MainModule.Types;
 
-        var winrt = !baseLibTypes.Any(type => type.Name == "Object");
+        var winrt = baseLibTypes.All(type => type.Name != "Object");
         if (winrt)
         {
             baseLib = assemblyResolver.Resolve("System.Runtime");
@@ -23,25 +23,25 @@ public static class ReferenceFinder
         }
 
         var argumentException = baseLibTypes.First(x => x.Name == "ArgumentException");
-        ArgumentExceptionConstructor = moduleDefinition.Import(argumentException.Methods.First(x =>
+        ArgumentExceptionConstructor = moduleDefinition.ImportReference(argumentException.Methods.First(x =>
             x.IsConstructor &&
             x.Parameters.Count == 2 &&
             x.Parameters[0].ParameterType.Name == "String" &&
             x.Parameters[1].ParameterType.Name == "String"));
 
         var argumentNullException = baseLibTypes.First(x => x.Name == "ArgumentNullException");
-        ArgumentNullExceptionConstructor = moduleDefinition.Import(argumentNullException.Methods.First(x =>
+        ArgumentNullExceptionConstructor = moduleDefinition.ImportReference(argumentNullException.Methods.First(x =>
             x.IsConstructor &&
             x.Parameters.Count == 1 &&
             x.Parameters[0].ParameterType.Name == "String"));
-        ArgumentNullExceptionWithMessageConstructor = moduleDefinition.Import(argumentNullException.Methods.First(x =>
+        ArgumentNullExceptionWithMessageConstructor = moduleDefinition.ImportReference(argumentNullException.Methods.First(x =>
             x.IsConstructor &&
             x.Parameters.Count == 2 &&
             x.Parameters[0].ParameterType.Name == "String" &&
             x.Parameters[1].ParameterType.Name == "String"));
 
         var invalidOperationException = baseLibTypes.First(x => x.Name == "InvalidOperationException");
-        InvalidOperationExceptionConstructor = moduleDefinition.Import(invalidOperationException.Methods.First(x =>
+        InvalidOperationExceptionConstructor = moduleDefinition.ImportReference(invalidOperationException.Methods.First(x =>
             x.IsConstructor &&
             x.Parameters.Count == 1 &&
             x.Parameters[0].ParameterType.Name == "String"));
@@ -50,7 +50,7 @@ public static class ReferenceFinder
         var debugLibTypes = debugLib.MainModule.Types;
 
         var debug = debugLibTypes.First(x => x.Name == "Debug");
-        DebugAssertMethod = moduleDefinition.Import(debug.Methods.First(x =>
+        DebugAssertMethod = moduleDefinition.ImportReference(debug.Methods.First(x =>
             x.IsStatic &&
             x.Parameters.Count == 2 &&
             x.Parameters[0].ParameterType.Name == "Boolean" &&
