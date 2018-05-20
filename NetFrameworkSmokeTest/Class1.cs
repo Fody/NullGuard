502de9afc942425beedcc371838c9eb8e2451ecd
tom-englert <mail@tom-englert.de>
2018-05-20 16:24:50 +0200

    Add smoke test for various target frameworks.

---
+++ NetFrameworkSmokeTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace NetFrameworkSmokeTest
+{
+    using NullGuard;
+
+    class Class1
+    {
+        public Class1(string something)
+        {
+
+        }
+
+        public string Test([AllowNull] string canbenull)
+        {
+            return default(string);
+        }
+
+        public string Test2(string notnull)
+        {
+            return default(string);
+        }
+    }
+}
