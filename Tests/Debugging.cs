1b7b80f9779bd1759b6782a66b8ab189d4abebdb
tom-englert <mail@tom-englert.de>
2018-09-04 19:51:08 +0200

    Add test to manually test debugging experience

---
+++ Tests/Debugging.cs
@@ -0,0 +1,21 @@
+﻿namespace Tests
+{
+    using System;
+
+    using NetFrameworkSmokeTest;
+
+    using Xunit;
+
+    public class Debugging
+    {
+        // [Fact] // just to manually step through the code and verify that breakpoints are hit properly.
+        void TestDebugging()
+        {
+            var c1 = new Class1("1");
+
+            Assert.Throws<InvalidOperationException>(() => c1.Test("2"));
+
+            c1.Test2("3");
+        }
+    }
+}
