99f78d8ac71d2cfa57fecf981c1e593f147ca1b8
David Gardiner <david@gardiner.net.au>
2019-11-30 08:38:42 +1030

    Supporting C# 8 Nullable Reference Type attributes
    - Method parameters and return values that use nullable reference types don't have null guards inserted
    
    Implements #203

---
+++ AssemblyToProcess/NullableReferenceTypeClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+public class NullableReferenceTypeClass
+{
+    /*
+    public void SomeMethod(string nonNullArg, [Nullable(2)] string nullArg)
+    {
+        Console.WriteLine(nonNullArg);
+    }
+    */
+    public void SomeMethod(string nonNullArg, string? nullArg)
+    {
+        Console.WriteLine(nonNullArg);
+    }
+
+    public string MethodWithReturnValue(bool returnNull)
+    {
+#pragma warning disable CS8603 // Possible null reference return.
+        return returnNull ? null : "";
+#pragma warning restore CS8603 // Possible null reference return.
+    }
+
+    /*
+    [NullableContext(2)]
+    public string MethodAllowsNullReturnValue()
+    {
+        return (string) null;
+    }
+    */
+    public string? MethodAllowsNullReturnValue()
+    {
+        return null;
+    }
+}
+#nullable disable
