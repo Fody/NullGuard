03d838cc38251392b9eff9513d5efe4f29a8e965
tom-englert <mail@tom-englert.de>
2020-01-08 18:26:28 +0100

    Support nullable attribute with byte[] constructor arg

--- AssemblyToProcessNullableRefTypes/ClassWithNullableContext1.cs
+++ AssemblyToProcessNullableRefTypes/ClassWithNullableContext1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
+using System;
 
 // Roslyn compiler adds [NullableContext(1)] because majority of methods do not use nullable reference types
 
@@ -40,15 +42,19 @@ public class ClassWithNullableContext1
 
     }
 
-    /*
-    [Nullable(2)]
-    public string NullProperty { [NullableContext(2)] get; [NullableContext(2)] set; }
-    */
+    // [Nullable(2)]
+    // public string NullProperty { [NullableContext(2)] get; [NullableContext(2)] set; }
     public string? NullProperty { get; set; }
 
-#pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
     public string NonNullProperty { get; set; }
-#pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
+    // [Nullable(new byte[] {2, 2, 1})]
+    // public Tuple<string, string> MixedNullProperty { [return: Nullable(new byte[] {2, 2, 1})] get; [param: Nullable(new byte[] {2, 2, 1})] set; }
+    public Tuple<string?, string>? MixedNullProperty { get; set; }
+
+    // [Nullable(new byte[] {1, 2, 1})]
+    // public Tuple<string, string> MixedNotNullProperty { [return: Nullable(new byte[] {1, 2, 1})] get; [param: Nullable(new byte[] {1, 2, 1})] set; }
+    public Tuple<string?, string> MixedNonNullProperty { get; set; }
 
 #nullable disable
     // [NullableContext(0)]
--- TestsNullableRefTypes/RewritingProperties.cs
+++ TestsNullableRefTypes/RewritingProperties.cs
@@ -93,3 +93,92 @@ public class RewritingProperties :
         Assert.Equal(value, sample.NonNullProperty);
     }
 }
+
+public class RewritingProperties2 :
+    VerifyBase
+{
+    public RewritingProperties2(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+
+    [Fact]
+    public void PropertySetterAllowsNullArgumentForNullableTypeInClassWithNullableContext1()
+    {
+        var sample = new ClassWithNullableContext1();
+        sample.MixedNullProperty = null;
+    }
+
+    [Fact]
+    public void PropertySetterAllowsNullArgumentForNullableTypeInClassWithNullableContext2()
+    {
+        var sample = new ClassWithNullableContext2();
+        sample.MixedNullProperty = null;
+    }
+
+    [Fact]
+    public void PropertyGetterReturnsNullForNullableTypeInClassWithNullableContext1()
+    {
+        var sample = new ClassWithNullableContext1();
+        Assert.Null(sample.MixedNullProperty);
+    }
+
+    [Fact]
+    public void PropertyGetterReturnsNullForNullableTypeInClassWithNullableContext2()
+    {
+        var sample = new ClassWithNullableContext2();
+        Assert.Null(sample.MixedNullProperty);
+    }
+
+    [Fact]
+    public void PropertySetterThrowsOnNullArgumentForNonNullableTypeInClassWithNullableContext1()
+    {
+        var sample = new ClassWithNullableContext1();
+        Assert.Throws<ArgumentNullException>(() => sample.MixedNonNullProperty = null);
+    }
+
+    [Fact]
+    public void PropertySetterThrowsOnNullArgumentForNonNullableTypeInClassWithNullableContext2()
+    {
+        var sample = new ClassWithNullableContext2();
+        Assert.Throws<ArgumentNullException>(() => sample.MixedNonNullProperty = null);
+    }
+
+    [Fact]
+    public void PropertyGetterThrowsOnNonNullableTypeInClassWithNullableContext1()
+    {
+        var sample = new ClassWithNullableContext1();
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            var dummy = sample.MixedNonNullProperty;
+        });
+    }
+
+    [Fact]
+    public void PropertyGetterThrowsOnNonNullableTypeInClassWithNullableContext2()
+    {
+        var sample = new ClassWithNullableContext2();
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            var dummy = sample.MixedNonNullProperty;
+        });
+    }
+
+    [Fact]
+    public void PropertyGetterReturnsValueForNonNullableTypeInClassWithNullableContext1()
+    {
+        var sample = new ClassWithNullableContext1();
+        var value = new Tuple<string, string>("a", "b");
+        sample.MixedNonNullProperty = value;
+        Assert.Equal(value, sample.MixedNonNullProperty);
+    }
+
+    [Fact]
+    public void PropertyGetterReturnsValueForNonNullableTypeInClassWithNullableContext2()
+    {
+        var sample = new ClassWithNullableContext2();
+        var value = new Tuple<string, string>("a", "b");
+        sample.MixedNonNullProperty = value;
+        Assert.Equal(value, sample.MixedNonNullProperty);
+    }
+}
