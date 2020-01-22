7e832e7a76c20a61c92f8789f52d7aa4601d49c8
Mike Marynowski <mikem@singulink.com>
2020-01-22 16:54:20 -0500

    Moved [MaybeNullTaskResult] to NullGuard.CodeAnalysis namespace
    Otherwise collisions will happen between NullGuard.AllowNull and System.Diagnostics.CodeAnalysis.AllowNullAttribute

--- AssemblyToProcessNullableRefTypes/ClassWithAsyncMethods.cs
+++ AssemblyToProcessNullableRefTypes/ClassWithAsyncMethods.cs
@@ -1,10 +1,8 @@
 ﻿#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using NullGuard;
+using NullGuard.CodeAnalysis;
 
 public class ClassWithAsyncMethods
 {
