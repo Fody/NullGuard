dd5c66ee1ac9b01207379bb06373fa6e1c0e90d4
Mike Marynowski <mikem@singulink.com>
2020-01-28 13:02:49 -0500

    Updated attribute XML docs with applicable mode

--- NullGuard/AllowNullAttribute.cs
+++ NullGuard/AllowNullAttribute.cs
@@ -3,7 +3,7 @@ using System;
 namespace NullGuard
 {
     /// <summary>
-    /// Prevents the injection of null checking.
+    /// Prevents the injection of null checking (implicit mode only).
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue | AttributeTargets.Property)]
     public class AllowNullAttribute : Attribute
