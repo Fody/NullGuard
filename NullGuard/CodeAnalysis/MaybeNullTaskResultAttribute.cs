a9f0979f59743d30bd6b7c4ec0d496125ac07776
Mike Marynowski <mikem@singulink.com>
2020-01-28 13:02:49 -0500

    Updated attribute XML docs with applicable mode

--- NullGuard/CodeAnalysis/MaybeNullTaskResultAttribute.cs
+++ NullGuard/CodeAnalysis/MaybeNullTaskResultAttribute.cs
@@ -3,7 +3,7 @@
 namespace NullGuard.CodeAnalysis
 {
     /// <summary>
-    /// Prevents injection of null checking on task result values when return value checks are enabled.
+    /// Prevents injection of null checking on task result values when return value checks are enabled (NRT mode only).
     /// </summary>
     [AttributeUsage(AttributeTargets.ReturnValue)]
     public class MaybeNullTaskResultAttribute : Attribute
